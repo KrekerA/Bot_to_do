c58a7bffb846dfa40746c1ea848927e3edc9710a
Kreker <angelikatum14@gmail.com>
2026-05-28 13:57:28 +0300

    Rename json.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import telebot
 from bot import TOKEN
-from json import save_tasks, read_tasks
+from func import save_tasks, read_tasks
 
 
 bot = telebot.TeleBot(TOKEN)# Создаем экземпляр бота с помощью токена
