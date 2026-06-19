164ad6a2687eec1157707ada305128011d1182b0
KrekerA <141130541+KrekerA@users.noreply.github.com>
2026-06-19 11:44:21 +0000

    Create website.py

---
+++ website.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+
+# Заголовок сайта
+st.set_page_config(page_title="Трекер Продуктивности", layout="wide")
+st.title("📊 Дашборд аналитики твоего обучения")
+st.subheader("Данные загружаются из твоего Telegram-бота")
