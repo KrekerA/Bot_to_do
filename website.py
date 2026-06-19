d643a38fdf0e7d96fefb8b68c313f869cdb080ad
KrekerA <141130541+KrekerA@users.noreply.github.com>
2026-06-19 11:48:55 +0000

    Connect db to a website

--- website.py
+++ website.py
@@ -1,7 +1,16 @@
+import sqlite3
 import streamlit as st
-
+from func import create_database, get_connection
 
 # Заголовок сайта
 st.set_page_config(page_title="Трекер Продуктивности", layout="wide")
 st.title("📊 Дашборд аналитики твоего обучения")
 st.subheader("Данные загружаются из твоего Telegram-бота")
+
+
+#  Подключает к бд и создает если её нет
+try:
+    get_connection()
+except:
+    create_database()
+    get_connection()
